6cd385d71c28438deeee625e32173a4c68c4db9a
tf8 <tf8@0ce99408-56cc-4101-9c47-0e61936f5c3b>
2010-09-21 09:47:10 +0000

    #5194490 Шедулер перенесён в крон.
    git-svn-id: http://212.158.164.130/svn/1234/trunk@28 0ce99408-56cc-4101-9c47-0e61936f5c3b

--- code/application/views/footer.php
+++ code/application/views/footer.php
@@ -1,34 +1,34 @@
-<?php 
+<?php
 /**
  * Footer view page.
  *
  * PHP version 5
- * LICENSE: This source file is subject to LGPL license 
+ * LICENSE: This source file is subject to LGPL license
  * that is available through the world-wide-web at the following URI:
  * http://www.gnu.org/copyleft/lesser.html
- * @author     Ushahidi Team <team@ushahidi.com> 
+ * @author     Ushahidi Team <team@ushahidi.com>
  * @package    Ushahidi - http://source.ushahididev.com
  * @module     API Controller
  * @copyright  Ushahidi - http://www.ushahidi.com
- * @license    http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL) 
+ * @license    http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
  */
 ?>
-     
- 
+
+
 	<!-- footer -->
 	<div id="footer" class="clearingfix">
- 
+
 		<div id="underfooter"></div>
- 
+
 		<!-- footer content -->
 		<div class="rapidxwpr floatholder">
- 
+
 			<!-- footer credits -->
 			<div class="footer-credits">
 				Powered by <a href="http://www.ushahidi.com/"><img src="<?php echo url::base(); ?>/media/img/footer-logo.png" alt="Ushahidi" align="absmiddle" /></a>
 			</div>
 			<!-- / footer credits -->
-		
+
 			<!-- footer menu -->
 			<div class="footermenu">
 				<ul class="clearingfix">
@@ -44,25 +44,25 @@
 			</div>
 			<!-- / footer menu -->
 
-      
+
 			<h2 class="feedback_title" style="clear:both">
 				<a href="http://feedback.ushahidi.com/fillsurvey.php?sid=5"><?php echo Kohana::lang('ui_main.feedback'); ?></a>
 			</h2>
 
- 
+
 		</div>
 		<!-- / footer content -->
- 
+
 	</div>
 	<!-- / footer -->
- 
+
 	<img src="<?php echo $tracker_url; ?>" />
 	<?php echo $ushahidi_stats; ?>
 	<?php echo $google_analytics; ?>
-	
+
 	<!-- Task Scheduler -->
-	<img src="<?php echo url::site() . 'scheduler'; ?>" height="1" width="1" border="0" />
- 
+	<!--  <img src="<?php echo url::site() . 'scheduler'; ?>" height="1" width="1" border="0" />
+
         <!-- script for share button -->
         <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pub=ushahidi"></script>
 </body>

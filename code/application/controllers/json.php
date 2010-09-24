aa9b0321918d6d847166d36aff2125b6f7b68191
MatveiFisenko <MatveiFisenko@0ce99408-56cc-4101-9c47-0e61936f5c3b>
2010-09-24 11:42:28 +0000

    Убраны пробелы.
    git-svn-id: http://212.158.164.130/svn/1234/trunk@52 0ce99408-56cc-4101-9c47-0e61936f5c3b

--- code/application/controllers/json.php
+++ code/application/controllers/json.php
@@ -4,20 +4,20 @@
  * Generates Map GeoJSON File
  *
  * PHP version 5
- * LICENSE: This source file is subject to LGPL license 
+ * LICENSE: This source file is subject to LGPL license
  * that is available through the world-wide-web at the following URI:
  * http://www.gnu.org/copyleft/lesser.html
- * @author     Ushahidi Team <team@ushahidi.com> 
+ * @author     Ushahidi Team <team@ushahidi.com>
  * @package    Ushahidi - http://source.ushahididev.com
- * @module     JSON Controller  
+ * @module     JSON Controller
  * @copyright  Ushahidi - http://www.ushahidi.com
- * @license    http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL) 
+ * @license    http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
  */
 
 class Json_Controller extends Template_Controller
 {
     public $auto_render = TRUE;
-	
+
     // Main template
     public $template = 'json';
 
@@ -27,12 +27,12 @@ class Json_Controller extends Template_Controller
 	public function __construct()
 	{
 		parent::__construct();
-		
+
 		// Set Table Prefix
 		$this->table_prefix = Kohana::config('database.default.table_prefix');
 	}
-	
-	
+
+
 	/**
 	 * Generate JSON in NON-CLUSTER mode
 	 */
@@ -49,7 +49,7 @@ class Json_Controller extends Template_Controller
 		$incident_id = "";
 		$neighboring = "";
 		$media_type = "";
-		
+
 		if (isset($_GET['c']) && !empty($_GET['c']))
 		{
 			$category_id = $_GET['c'];
@@ -60,17 +60,17 @@ class Json_Controller extends Template_Controller
 				                                ->find()->id;
 			}
 		}
-		
+
 		if (isset($_GET['i']) && !empty($_GET['i']))
 		{
 			$incident_id = $_GET['i'];
 		}
-		
+
 		if (isset($_GET['n']) && !empty($_GET['n']))
 		{
 			$neighboring = $_GET['n'];
 		}
-		
+
 		$where_text = '';
 		// Do we have a media id to filter by?
 		if (isset($_GET['m']) && !empty($_GET['m']) && $_GET['m'] != '0')
@@ -78,19 +78,19 @@ class Json_Controller extends Template_Controller
 			$media_type = $_GET['m'];
 			$where_text .= " AND ".$this->table_prefix."media.media_type = " . $media_type;
 		}
-		
+
         if (isset($_GET['s']) && !empty($_GET['s']))
 		{
-        	$start_date = $_GET['s']; 
+        	$start_date = $_GET['s'];
         	$where_text .= " AND UNIX_TIMESTAMP(".$this->table_prefix."incident.incident_date) >= '" . $start_date . "'";
         }
-        
+
 		if (isset($_GET['e']) && !empty($_GET['e']))
 		{
-        	$end_date = $_GET['e']; 
+        	$end_date = $_GET['e'];
         	$where_text .= " AND UNIX_TIMESTAMP(".$this->table_prefix."incident.incident_date) <= '" . $end_date . "'";
         }
-                
+
         // Do we have a category id to filter by?
         if (is_numeric($category_id) && $category_id != '0')
         {
@@ -98,7 +98,7 @@ class Json_Controller extends Template_Controller
 			$category = ORM::factory('category', $category_id);
             $color = $category->category_color;
 			$icon = $category->category_image;
-			
+
 			$where_child = "";
 			$children = ORM::factory('category')
 				->where('parent_id', $category_id)
@@ -107,7 +107,7 @@ class Json_Controller extends Template_Controller
 			{
 				$where_child .= " OR incident_category.category_id = ".$child->id." ";
 			}
-			
+
             // Retrieve markers by category
             // XXX: Might need to replace magic numbers
 			$markers = ORM::factory('incident')
@@ -117,8 +117,8 @@ class Json_Controller extends Template_Controller
 				->join('media', 'incident.id', 'media.incident_id','LEFT')
 				->where('incident.incident_active = 1 AND ('.$this->table_prefix.'incident_category.category_id = ' . $category_id . ' ' . $where_child . ')' . $where_text)
 				->find_all();
-			
-                     
+
+
         }
         else
         {
@@ -130,22 +130,22 @@ class Json_Controller extends Template_Controller
 				->where('incident.incident_active = 1 '.$where_text)
 				->find_all();
         }
-        
+
 		$json_item_first = "";	// Variable to store individual item for report detail page
         foreach ($markers as $marker)
-        {	
+        {
             $json_item = "{";
             $json_item .= "\"type\":\"Feature\",";
             $json_item .= "\"properties\": {";
 			$json_item .= "\"id\": \"".$marker->id."\", \n";
             $json_item .= "\"name\":\"" . str_replace(chr(10), ' ', str_replace(chr(13), ' ', "<a href='" . url::base() . "reports/view/" . $marker->id . "'>" . htmlentities($marker->incident_title) . "</a>")) . "\",";
-			
-			if (isset($category)) { 
+
+			if (isset($category)) {
 				$json_item .= "\"category\":[" . $category_id . "], ";
 			} else {
 				$json_item .= "\"category\":[0], ";
 			}
-			
+
 			$json_item .= "\"color\": \"".$color."\", \n";
 			$json_item .= "\"icon\": \"".$icon."\", \n";
             $json_item .= "\"timestamp\": \"" . strtotime($marker->incident_date) . "\"";
@@ -155,7 +155,7 @@ class Json_Controller extends Template_Controller
             $json_item .= "\"coordinates\":[" . $marker->location->longitude . ", " . $marker->location->latitude . "]";
             $json_item .= "}";
             $json_item .= "}";
-		
+
 			if ($marker->id == $incident_id)
 			{
 				$json_item_first = $json_item;
@@ -176,7 +176,7 @@ class Json_Controller extends Template_Controller
         $this->template->json = $json;
     }
 
-	
+
 	/**
 	 * Generate JSON in CLUSTER mode
 	 */
@@ -200,7 +200,7 @@ class Json_Controller extends Template_Controller
 		$distance = (10000000 >> $zoomLevel) / 100000;
 
 		// Category ID
-		$category_id = (isset($_GET['c']) && !empty($_GET['c']) && 
+		$category_id = (isset($_GET['c']) && !empty($_GET['c']) &&
 			is_numeric($_GET['c']) && $_GET['c'] != 0) ?
 			$_GET['c'] : 0;
 
@@ -217,14 +217,14 @@ class Json_Controller extends Template_Controller
 			$_GET['sw'] : "0";
 
 		$northeast = (isset($_GET['ne']) && !empty($_GET['ne'])) ?
-			$_GET['ne'] : "0";			
+			$_GET['ne'] : "0";
 
 		$filter = "";
 		$filter .= ($category_id !=0) ? " AND ( c.id=".$category_id
 			." OR c.parent_id=".$category_id.") " : "";
-		$filter .= ($start_date) ? 
+		$filter .= ($start_date) ?
 			" AND i.incident_date >= '" . date("Y-m-d H:i:s", $start_date) . "'" : "";
-		$filter .= ($end_date) ? 
+		$filter .= ($end_date) ?
 			" AND i.incident_date <= '" . date("Y-m-d H:i:s", $end_date) . "'" : "";
 
 		if ($southwest && $northeast)
@@ -248,7 +248,7 @@ class Json_Controller extends Template_Controller
 			}
 		}
 
-		$query = $db->query("SELECT DISTINCT i.id, i.incident_title, l.`latitude`, l.`longitude` FROM `".$this->table_prefix."incident` AS i INNER JOIN `".$this->table_prefix."location` AS l ON (l.`id` = i.`location_id`) INNER JOIN `".$this->table_prefix."incident_category` AS ic ON (i.`id` = ic.`incident_id`) INNER JOIN `".$this->table_prefix."category` AS c ON (ic.`category_id` = c.`id`) WHERE i.incident_active=1 $filter ORDER BY i.`id` ASC ");	
+		$query = $db->query("SELECT DISTINCT i.id, i.incident_title, l.`latitude`, l.`longitude` FROM `".$this->table_prefix."incident` AS i INNER JOIN `".$this->table_prefix."location` AS l ON (l.`id` = i.`location_id`) INNER JOIN `".$this->table_prefix."incident_category` AS ic ON (i.`id` = ic.`incident_id`) INNER JOIN `".$this->table_prefix."category` AS c ON (ic.`category_id` = c.`id`) WHERE i.incident_active=1 $filter ORDER BY i.`id` ASC ");
 
 		$query->result(FALSE, MYSQL_ASSOC);
 
@@ -273,10 +273,10 @@ class Json_Controller extends Template_Controller
 			foreach ($markers as $key => $target)
 			{
 				// This function returns the distance between two markers, at a defined zoom level.
-				// $pixels = $this->_pixelDistance($marker['latitude'], $marker['longitude'], 
+				// $pixels = $this->_pixelDistance($marker['latitude'], $marker['longitude'],
 				// $target['latitude'], $target['longitude'], $zoomLevel);
 
-				$pixels = abs($marker['longitude']-$target['longitude']) + 
+				$pixels = abs($marker['longitude']-$target['longitude']) +
 					abs($marker['latitude']-$target['latitude']);
 				// echo $pixels."<BR>";
 				// If two markers are closer than defined distance, remove compareMarker from array and add to cluster.
@@ -315,7 +315,7 @@ class Json_Controller extends Template_Controller
 			$json_item = "{";
 		    $json_item .= "\"type\":\"Feature\",";
 		    $json_item .= "\"properties\": {";
-			$json_item .= "\"name\":\"" . str_replace(chr(10), ' ', str_replace(chr(13), ' ', "<a href=" . url::base() . "reports/index/?c=".$category_id."&sw=".$southwest."&ne=".$northeast.">" . $cluster_count . " Reports</a>")) . "\",";			
+			$json_item .= "\"name\":\"" . str_replace(chr(10), ' ', str_replace(chr(13), ' ', "<a href=" . url::base() . "reports/index/?c=".$category_id."&sw=".$southwest."&ne=".$northeast.">" . $cluster_count . " Reports</a>")) . "\",";
 		    $json_item .= "\"category\":[0], ";
 			$json_item .= "\"color\": \"".$color."\", ";
 			$json_item .= "\"icon\": \"".$icon."\", ";
@@ -336,7 +336,7 @@ class Json_Controller extends Template_Controller
 			$json_item = "{";
 		    $json_item .= "\"type\":\"Feature\",";
 		    $json_item .= "\"properties\": {";
-			$json_item .= "\"name\":\"" . str_replace(chr(10), ' ', str_replace(chr(13), ' ', "<a href=" . url::base() . "reports/view/" . $single['id'] . "/>".$single['incident_title']."</a>")) . "\",";	
+			$json_item .= "\"name\":\"" . str_replace(chr(10), ' ', str_replace(chr(13), ' ', "<a href=" . url::base() . "reports/view/" . $single['id'] . "/>".$single['incident_title']."</a>")) . "\",";
 		    $json_item .= "\"category\":[0], ";
 			$json_item .= "\"color\": \"".$color."\", ";
 			$json_item .= "\"icon\": \"".$icon."\", ";
@@ -366,7 +366,7 @@ class Json_Controller extends Template_Controller
 	{
 		$json = "";
 		$json_item = "";
-		$json_array = array();	
+		$json_array = array();
 
 
 		$marker = ORM::factory('incident')
@@ -389,9 +389,9 @@ class Json_Controller extends Template_Controller
 			$db = new Database();
 
 			// Get Neighboring Markers Within 50 Kms (31 Miles)
-			$query = $db->query("SELECT DISTINCT i.*, l.`latitude`, l.`longitude`, 
-			((ACOS(SIN($latitude * PI() / 180) * SIN(l.`latitude` * PI() / 180) + COS($latitude * PI() / 180) * COS(l.`latitude` * PI() / 180) * COS(($longitude - l.`longitude`) * PI() / 180)) * 180 / PI()) * 60 * 1.1515) AS distance 
-			 FROM `".$this->table_prefix."incident` AS i INNER JOIN `".$this->table_prefix."location` AS l ON (l.`id` = i.`location_id`) INNER JOIN `".$this->table_prefix."incident_category` AS ic ON (i.`id` = ic.`incident_id`) INNER JOIN `".$this->table_prefix."category` AS c ON (ic.`category_id` = c.`id`) WHERE i.incident_active=1 $filter 
+			$query = $db->query("SELECT DISTINCT i.*, l.`latitude`, l.`longitude`,
+			((ACOS(SIN($latitude * PI() / 180) * SIN(l.`latitude` * PI() / 180) + COS($latitude * PI() / 180) * COS(l.`latitude` * PI() / 180) * COS(($longitude - l.`longitude`) * PI() / 180)) * 180 / PI()) * 60 * 1.1515) AS distance
+			 FROM `".$this->table_prefix."incident` AS i INNER JOIN `".$this->table_prefix."location` AS l ON (l.`id` = i.`location_id`) INNER JOIN `".$this->table_prefix."incident_category` AS ic ON (i.`id` = ic.`incident_id`) INNER JOIN `".$this->table_prefix."category` AS c ON (ic.`category_id` = c.`id`) WHERE i.incident_active=1 $filter
 			HAVING distance<='62'
 			 ORDER BY i.`id` ASC ");
 
@@ -437,7 +437,7 @@ class Json_Controller extends Template_Controller
 		header('Content-type: application/json');
 		$this->template->json = $json;
 	}
-    
+
 	/**
 	 * Retrieve timeline JSON
 	 */
@@ -445,7 +445,7 @@ class Json_Controller extends Template_Controller
 	{
         $this->auto_render = FALSE;
         $this->template = new View('json/timeline');
-        
+
         $interval = 'day';
         $start_date = NULL;
         $end_date = NULL;
@@ -481,22 +481,22 @@ class Json_Controller extends Template_Controller
 	{
 		$this->template = "";
 		$this->auto_render = FALSE;
-		
+
 		$layer = ORM::factory('layer')
 			->where('layer_visible', 1)
 			->find($layer_id);
-		
+
 		if ($layer->loaded)
 		{
 			$layer_url = $layer->layer_url;
 			$layer_file = $layer->layer_file;
-			
+
 			$layer_link = (!$layer_url) ?
 				url::base().'media/uploads/'.$layer_file :
 				$layer_url;
-			
+
 			$content = file_get_contents($layer_link);
-			
+
 			if ($content !== false)
 			{
 				echo $content;
@@ -520,12 +520,12 @@ class Json_Controller extends Template_Controller
 	{
 		$this->template = "";
 		$this->auto_render = FALSE;
-		
+
 		$sharing_data = "";
 		if ($share_id)
 		{
 			$cache = Cache::instance();
-			
+
 			$share = ORM::factory('sharing', $share_id)
 				->find();
 			if ($share->loaded)
@@ -533,18 +533,18 @@ class Json_Controller extends Template_Controller
 				$sharing_key = $share->sharing_key;
 				$sharing_color = $share->sharing_color;
 				$sharing_cache = $share->id."_".$sharing_key;
-				
+
 				$sharing_data = utf8_decode($cache->get($sharing_cache));
-				
+
 				// Perform color replacement
 				$sharing_data = str_replace("%%%COLOR%%%", $sharing_color, $sharing_data);
 			}
 		}
-		
+
 		header('Content-type: application/json');
 		echo $sharing_data;
 	}
-	
+
 	/**
 	 * Convert Longitude to Cartesian (Pixels) value
 	 * @param double $lon - Longitude
@@ -552,7 +552,7 @@ class Json_Controller extends Template_Controller
 	 */
 	private function _lonToX($lon)
 	{
-	    return round(OFFSET + RADIUS * $lon * pi() / 180);        
+	    return round(OFFSET + RADIUS * $lon * pi() / 180);
 	}
 
 	/**
@@ -562,8 +562,8 @@ class Json_Controller extends Template_Controller
 	 */
 	private function _latToY($lat)
 	{
-	    return round(OFFSET - RADIUS * 
-	                log((1 + sin($lat * pi() / 180)) / 
+	    return round(OFFSET - RADIUS *
+	                log((1 + sin($lat * pi() / 180)) /
 	                (1 - sin($lat * pi() / 180))) / 2);
 	}
 
@@ -585,12 +585,12 @@ class Json_Controller extends Template_Controller
 
 	    return sqrt(pow(($x1-$x2),2) + pow(($y1-$y2),2)) >> (21 - $zoom);
 	}
-	
+
 	/**
 	 * Calculate the center of a cluster of markers
 	 * @param array $cluster
 	 * @return array - (center, southwest bound, northeast bound)
-	 */	
+	 */
     private function _calculateCenter($cluster)
 	{
 		// Calculate average lat and lon of clustered items
@@ -598,7 +598,7 @@ class Json_Controller extends Template_Controller
 		$west = 0;
 		$north = 0;
 		$east = 0;
-		
+
         $lat_sum = $lon_sum = 0;
         foreach ($cluster as $marker)
 		{
@@ -610,7 +610,7 @@ class Json_Controller extends Template_Controller
 			{
 				$south = $marker['latitude'];
 			}
-				
+
 			if (!$west)
 			{
 				$west = $marker['longitude'];
@@ -619,7 +619,7 @@ class Json_Controller extends Template_Controller
 			{
 				$west = $marker['longitude'];
 			}
-			
+
 			if (!$north)
 			{
 				$north = $marker['latitude'];
@@ -628,7 +628,7 @@ class Json_Controller extends Template_Controller
 			{
 				$north = $marker['latitude'];
 			}
-			
+
 			if (!$east)
 			{
 				$east = $marker['longitude'];
@@ -637,17 +637,17 @@ class Json_Controller extends Template_Controller
 			{
 				$east = $marker['longitude'];
 			}
-			
+
 			$lat_sum += $marker['latitude'];
 			$lon_sum += $marker['longitude'];
         }
 		$lat_avg = $lat_sum / count($cluster);
 		$lon_avg = $lon_sum / count($cluster);
-		
+
 		$center = $lon_avg.",".$lat_avg;
 		$sw = $west.",".$south;
 		$ne = $east.",".$north;
-		
+
 		return array(
 			"center"=>$center,
 			"sw"=>$sw,

ebb4316ecc92f8bd89728b27d2fd107f405ee089
MatveiFisenko <MatveiFisenko@0ce99408-56cc-4101-9c47-0e61936f5c3b>
2010-10-28 11:39:51 +0000

    Google Analytics.
    git-svn-id: http://212.158.164.130/svn/1234/trunk@107 0ce99408-56cc-4101-9c47-0e61936f5c3b

--- code/application/views/footer.php
+++ code/application/views/footer.php
@@ -56,6 +56,22 @@
 	<!-- Task Scheduler -->
 	<!--  <img src="<?php echo url::site() . 'scheduler'; ?>" height="1" width="1" border="0" /> -->
 
+<!-- Google Analytics-->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-9404395-4']);
+  _gaq.push(['_setDomainName', '.st1234.ru']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
         <!-- script for share button -->
         <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pub=ushahidi"></script>
 
